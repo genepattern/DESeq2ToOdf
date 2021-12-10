a9a55986133b2aa0faf8f9f88b934a22c7a22037
Alex Wenzel <atwenzel@eng.ucsd.edu>
2021-12-09 22:02:44 -0800

    added parameterization for id and stat columns

--- src/txt2odf.py
+++ src/txt2odf.py
@@ -3,15 +3,22 @@ from txt2odf_functions import *
 import os
 
 print(sys.argv)
-txt_file, prune_gct, gct, cls = parse_arguments(sys.argv)
+#txt_file, prune_gct, gct, cls = parse_arguments(sys.argv)
+txt_file, id_col, stat_col, prune_gct, gct, cls = parse_arguments(sys.argv)
 
 name_of_file = os.path.basename(txt_file).strip('txt')+'odf'
 
 data_df = pd.read_table(txt_file)
-data_df.insert(1, 'Description', data_df['id'])
+#data_df.insert(1, 'Description', data_df['id'])
+data_df.insert(1, 'Description', data_df[id_col])
 data_df.insert(0, 'Rank', data_df.index.values+1)
-data_df.insert(3, 'Score', data_df['stat'])
-data_df.rename(columns={'id': 'Feature'}, inplace=True)
+#data_df.insert(3, 'Score', data_df['stat'])
+data_df.insert(3, 'Score', data_df[stat_col])
+#data_df.rename(columns={'id': 'Feature'}, inplace=True)
+data_df.rename(columns={id_col: 'Feature'}, inplace = True)
+
+rest_of_cols = [col for col in data_df.columns if col not in ["Rank", "Feature"]]
+data_df = data_df[["Rank", "Feature"] + rest_of_cols]
 
 classes = ['Nothing', 'class_0', 'class_1']
 if cls is not None:
@@ -27,7 +34,8 @@ vals['cls'] = txt_file
 vals['n_perm'] = 1
 vals['class_0'] = classes[2]
 vals['class_1'] = classes[1]
-vals['func'] = 'stat'
+#vals['func'] = 'stat'
+vals['func'] = stat_col
 vals['rand_seed'] = 123456789
 vals['dat_lines'] = len(data_df)
 
@@ -37,10 +45,16 @@ print('\tFile', name_of_file, "has been written!")
 if prune_gct:
     original_gct = pd.read_table(gct, sep='\t', skiprows=2)
     # print(original_gct)
-    df = original_gct.loc[original_gct['Name'].isin(data_df['Feature'])]
+    #df = original_gct.loc[original_gct['Name'].isin(data_df['Feature'])]
+    try:
+        df = original_gct.loc[original_gct["Name"].isin(data_df["Feature"])]
+    except KeyError:
+        df = original_gct.loc[original_gct["NAME"].isin(data_df["Feature"])]
+
     pruned = open(name_of_file.strip('.odf')+'_pruned.gct', 'w')
     pruned.write("#1.2\n")
-    pruned.write("\t".join([str(df.shape[0]), str(df.shape[1])]) + "\n")
+    #pruned.write("\t".join([str(df.shape[0]), str(df.shape[1])]) + "\n")
+    pruned.write("\t".join([str(df.shape[0]), str(df.shape[1] - 2)]) + "\n")
     pruned.write(df.to_csv(sep='\t', index=False))
     pruned.close()
 
